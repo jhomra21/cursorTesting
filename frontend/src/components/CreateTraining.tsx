15675413f8efb20b78642838b7db4f8234c39e9c
Juan <jhonra@icloud.com>
2024-09-23 19:05:28 -0500

    updating training page

--- frontend/src/components/CreateTraining.tsx
+++ frontend/src/components/CreateTraining.tsx
@@ -1,7 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { User, Model } from '../types';
+import { Button, buttonVariants } from './ui/button';
+import { Input } from './ui/input';
+import { Slider } from './ui/slider';
+import { DownloadIcon, ReloadIcon } from '@radix-ui/react-icons';
+import { Link } from 'react-router-dom';
+
 export default function CreateTraining() {
     return (
-        <div>
-            <h1>Create Training</h1>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-2xl font-bold">Create Training</h1>
+            <Link to="/" className={buttonVariants({ variant: "outline" }) + " m-2 hover:bg-gray-900 hover:text-white"}>Back</Link>
         </div>
     )
 }
